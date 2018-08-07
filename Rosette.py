1cffc746415876429be2f9087cbeecf62cae32e5
byzhang001 <40659224+byzhang001@users.noreply.github.com>
2018-08-07 17:40:57 +0800

    BombCatcher

---
+++ Rosette.py
@@ -0,0 +1,11 @@
+import turtle
+
+t=turtle.Pen()
+t.circle(100)
+t.left(90)
+t.circle(100)
+t.left(90)
+t.circle(100)
+t.left(90)
+t.circle(100)
+t.left(90)
---
+++ SayMyName.py
@@ -0,0 +1,6 @@
+
+
+name=input("What's your name?")
+for x in range(100):
+    print(name,end="! ")
+    
