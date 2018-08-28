0eef3276ab50d6c8621cc0821415526343629d90
byzhang001 <40659224+byzhang001@users.noreply.github.com>
2018-08-28 16:27:02 +0800

    BombCatcher

--- Test.py
+++ Test.py
@@ -2,15 +2,23 @@ import  sys
 import datetime
 from datetime import datetime,timezone
 
+a,b,c=1,2,3
+print(a,b,c)
 print(sys.copyright)
 print(sys.platform)
 print(sys.version)
 
 print(datetime.now())
-strTemp="""
-    Three Rings for the elven kings under the sky,
-    Seven for the dwarf lords in their halls of stone
-"""
-print(strTemp)
-name=input("What is your name?")
-print("Name is : " + name)
+# strTemp="""
+#     Three Rings for the elven kings under the sky,
+#     Seven for the dwarf lords in their halls of stone
+# """
+# print(strTemp)
+# name=input("What is your name?")
+# print("Name is : " + name)
+for x in range(100):
+    if  (x%2==1):
+        print(x)
+
+print("Test",end="")
+print("End")
